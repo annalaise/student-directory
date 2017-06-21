5bb0077dd6cf8946a1dac0015b5ac5ba503c44f5
annalaise <annalaise.gibbons@gmail.com>
2017-06-21 19:02:33 +0800

    adding methods to directory to simplify future modification of standard output

--- directory.rb
+++ directory.rb
@@ -14,11 +14,21 @@ students = [
 "Norman Bates"
 ]
 
-# these can then be printed from the array using the array index
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-   puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
-# finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+
+def print(names)
+  names.each do |name|
+   puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
