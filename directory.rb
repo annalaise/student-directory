ca0c1abef7d187cc21c1e1fd118c6d55145a9a81
annalaise <annalaise.gibbons@gmail.com>
2017-06-23 10:23:13 +0800

    Refactored exercise 8:5, iterating into array hash

--- directory.rb
+++ directory.rb
@@ -4,10 +4,10 @@ def input_students
   name = gets.chomp
   while !name.empty? do
     puts "What is the student's nationality?"
-    nationality = gets.chomp
+    nat = gets.chomp
     puts "What is the student's current age?"
     age = gets.chomp
-    students << {name: name, cohort: :november, nationality: nationality, age: age}
+    students << {name: name, cohort: :november, nat: nat, age: age}
     puts "Now we have #{students.count} students"
     puts "Enter another student, or hit return to finish"
     name = gets.chomp
@@ -18,22 +18,21 @@ end
 def print_header
   line1 = "The students of Villains Academy"
   line2 = "-------------"
-  puts line1.center(15)
-  puts line2.center(15)
+  puts line1.center(20)
+  puts line2.center(20)
 end
 
 def print(students)
-  count = 0
-  while count < students.count
-    studentline = "#{count+1}. #{students[count][:name]} (#{students[count][:cohort]} cohort) Age:#{students[count][:age]} Nationality:#{students[count][:nationality]}"
-    puts studentline.center(15)
-    count += 1
+    students.each_with_index do |student, i|
+      if student.class == Hash
+        puts "#{i+1}. #{students[i][:name]} (#{students[i][:cohort]} cohort) Age: #{students[i][:age]} Nationality: #{students[i][:nat]}"
+      end
   end
 end
 
 def print_footer(students)
   footer = "Overall, we have #{students.count} great students"
-  puts footer.center(15)
+  puts footer.center(20)
 end
 
 students = input_students
