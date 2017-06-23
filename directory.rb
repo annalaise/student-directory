4f81f5a6ed62ea837a46c1622ac8c787fdcb745e
annalaise <annalaise.gibbons@gmail.com>
2017-06-23 11:22:18 +0800

    refactoring centre align method

--- directory.rb
+++ directory.rb
@@ -1,35 +1,40 @@
+def center_puts(string)
+  width = 50
+  puts string.center(width)
+end
+
 def input_students
-  puts "Please enter the name of the student"
+  center_puts("Please enter the name of the student")
   students = []
   name = gets.chomp
   while !name.empty? do
-    puts "What is the student's nationality?"
-    nat = gets.chomp
-    puts "What is the student's current age?"
+    center_puts("Where is #{name} from?")
+    country = gets.chomp
+    center_puts("How old is #{name}?")
     age = gets.chomp
-    students << {name: name, cohort: :november, nat: nat, age: age}
-    puts "Now we have #{students.count} students"
-    puts "Enter another student, or hit return to finish"
+    students << {name: name.capitalize, cohort: :november, country: country.capitalize, age: age}
+    center_puts("Now we have #{students.count} students")
+    center_puts("Enter another student, or hit return to finish")
     name = gets.chomp
   end
   students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  center_puts("The students of Villains Academy")
+  center_puts("-------------")
 end
 
 def print(students)
     students.each_with_index do |student, i|
       if student.class == Hash
-      puts "#{(i+1)}. #{(students[i][:name]).rjust(15)} (#{students[i][:cohort]} cohort) Age: #{students[i][:age]} Nationality: #{students[i][:nat]}"
+      center_puts("#{(i+1)}. #{(students[i][:name])} (#{students[i][:cohort]} cohort) Age: #{students[i][:age]} Country: #{students[i][:country]}")
       end
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  center_puts("Overall, we have #{students.count} great students")
 end
 
 students = input_students
