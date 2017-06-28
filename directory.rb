f01bc0e114634b834042889ad7baca28ce9189dc
annalaise <annalaise.gibbons@gmail.com>
2017-06-28 12:10:00 +0800

    adding fucntion to save student data to file, and execute gitignore on it =

--- directory.rb
+++ directory.rb
@@ -14,6 +14,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -24,13 +26,8 @@ end
 def print_menu
   puts "1. Enter new student details"
   puts "2. Display existing student details"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
-end
-
-def show_students
-  print_header
-  print_student_list
-  print_footer
 end
 
 # Collects student details from user
@@ -59,6 +56,24 @@ def input_students
     puts "Enter another student, or hit return to finish"
     name = gets.chomp
   end
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 # Prints header for list of students, only if student list is not empty
